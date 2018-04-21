e275d1c3680f656ed4feeee4cfea8a01e2beca8b
farid <farid.lfsibgtu.ru>
2018-04-21 19:08:07 +0700

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'statistic',
+    'password' => 'lViw0V5LHLNnWljJ',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
